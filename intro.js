bbf00dc1364a9fa765daa61ea289c68f653716d4
Sebastian Cain <sebastiancain@Sebastians-MacBook-Pro.local>
2016-12-15 14:06:02 -0500

    after class thurs

--- intro.js
+++ intro.js
@@ -1,39 +1,3 @@
-//send diagnostic output to console
-//(Ctrl-Shift-J in Chromium & Firefox to reveal console)
-console.log("AYO");
-
-var i = "hello";
-var j = 20;
-
-
-//assign an anonymous fxn to a var
-var f = function(x) {
-    var j=30;
-    return j+x;
-};
-
-
-//(define fact (lambda (n) ...)
-var fact = function(n) {
-    var prod=1;
-    for ( ; n > 1 ; n--){
-	prod = prod * n;
-    }
-    return prod;
-};
-
-
-//(define fact (lambda (n) ...)
-var factR = function(n) {
-    if ( n <= 1 ) {
-	return 1;
-    }
-    else {
-	return n * factR(n-1);
-    }
-};
-
-
 //add list item 
 var addItem = function(text) {
     var list = document.getElementById("thelist");
@@ -47,45 +11,24 @@ var addItem = function(text) {
 var removeItem = function(n) {
     var listitems = document.getElementsByTagName('li');
     listitems[n].remove();
+    console.log("remove items");
 };
 
-
-var red = function() {
-    var items = document.getElementsByTagName("li");
-    for(var i = 0; i < items.length; i++) {
-	items[i].classList.add('red');
-    }
-};
-
-
-//instantiate an object
-var o = { 'name' : 'Thluffy',
-	  age : 15,
-	  items : [10, 20, 30, 40],
-	  morestuff : {a : 1, b : 'ayo'},
-	  func : function(x) {
-	      return x+30;
-	  }
-};
-
-var buttonCallback = function (e) {
-    console.log(e);
-    console.log(this);
+var items = document.getElementsByTagName("li");
+for(var i = 0; i < items.length; i++) {
+    items[i].addEventListener("click", deleteCallback);
 }
 
 var b = document.getElementById("b");
-b.addEventListener("click", buttonCallback);
 
-var redCallback = function(e) {
+var deleteCallback = function(e) {
     console.log(this);
-    this.classList.add("red");
+    this.remove()
 };
-
-b.addEventListener("click", redCallback);
 
 var buttonAdd = function(k) {
     console.log(this);
-    addItem("ayo");
+    addItem("item " + document.getElementsByTagName("li").length);
 };
 
 b.addEventListener("click", buttonAdd);
