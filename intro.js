9311e233d144dc5cd355b10c0cf0351387c575f2
Lawrence Liu <lawrence255@yahoo.com>
2016-12-16 14:00:57 -0500

    Phase 1 Done

--- intro.js
+++ intro.js
@@ -1,9 +1,12 @@
 //add list item 
 var addItem = function(text) {
     var list = document.getElementById("thelist");
-    var newitem = document.createElement("li");
-    newitem.innerHTML = text;
-    list.appendChild(newitem);
+    var newItem = document.createElement("li");
+    newItem.innerHTML = text;
+    newItem.addEventListener("mouseover", head);
+    newItem.addEventListener("mouseout", helloWorld);
+    newItem.addEventListener("click", deleteCallback);
+    list.appendChild(newItem);
 };
 
 var buttonAdd = function(k) {
@@ -14,36 +17,29 @@ var b = document.getElementById("b");
 
 b.addEventListener("click", buttonAdd);
 
+
 var deleteCallback = function(e) {
     this.remove()
 };
 
-var removeStuff = function (x) {
-    var items = document.getElementsByTagName("li");
-    for(var i = 0; i < items.length; i++) {
-	items[i].addEventListener("click", deleteCallback);
-    }
-};
-
-b.addEventListener("click", removeStuff);
+var items = document.getElementsByTagName("li");
+for(var i = 0; i < items.length; i++) {
+    items[i].addEventListener("click", deleteCallback);
+}
 
 
 var head = function (z) {
     console.log();
-    document.getElementById("h").innerHTML = "lol idk what goes here";
+    document.getElementById("h").innerHTML = this.innerHTML;
 };
-
-var showHead = function (y) {
-    var items = document.getElementsByTagName("li");
-    for(var i = 0; i < items.length; i++) {
-	items[i].addEventListener("mouseover", head);
-    }
-};
-
-b.addEventListener("mouseover", showHead);
 
 var helloWorld = function (q) {
     document.getElementById("h").innerHTML="Hello World!";
 };
 
-b.addEventListener("mouseout", helloWorld);
+var items = document.getElementsByTagName("li");
+for(var i = 0; i < items.length; i++) {
+    items[i].addEventListener("mouseover", head);
+    items[i].addEventListener("mouseout", helloWorld);
+}
+
